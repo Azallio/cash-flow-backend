aa2e7342227bcc2347271f664ebd9b289aeb91ff
Azallio <allio.all.in.one@gmail.com>
2026-05-31 15:44:14 +0300

    fix: fix auth module imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,14 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from '../common/middlewares/guards/strategies/jwtStrategy';
 import { RefreshTokenEntity } from '../DAL/entities/refresh-token.entity';
-import { UsersModule } from '../user/user.module';
+import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtService } from './jwt.service';
 
 @Module({
   imports: [
-    UsersModule,
+    UserModule,
     MikroOrmModule.forFeature([RefreshTokenEntity]),
     JwtModule.register({
       global: true,
